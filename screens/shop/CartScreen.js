2c6e4c44f890e2b2585fb0790af64428d5faa32c
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-05-24 20:26:31 +0530

    clear cart and side navigation bar

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -44,6 +44,10 @@ const CartScreen = props => {
     </View>
 };
 
+CartScreen.navigationOptions = {
+    headerTitle:'Your Cart'
+};
+
 const styles = StyleSheet.create({
     screen:{
         margin:20,
