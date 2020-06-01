bff8882292fc636a0edf49cfeae6f714bdd2ef0c
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-06-01 23:18:20 +0530

    Dispatching Actions for Creating &

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,91 +1,130 @@
-import React,{useState,useEffect,useCallback} from 'react';
-import { View, Text, StyleSheet, TextInput, ScrollView ,Platform} from 'react-native';
-import HeaderButtonComponent from '../../components/UI/HeaderButton';
-import {HeaderButtons,Item} from 'react-navigation-header-buttons';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  StyleSheet,
+  Platform
+} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector, useDispatch } from 'react-redux';
+
+import HeaderButton from '../../components/UI/HeaderButton';
+import * as productsActions from '../../store/actions/products';
+
 const EditProductScreen = props => {
+  const prodId = props.navigation.getParam('productId');
+  const editedProduct = useSelector(state =>
+    state.products.userProducts.find(prod => prod.id === prodId)
+  );
+  const dispatch = useDispatch();
 
-    
-const prodId = props.navigation.getParam('ProductId');
-const editedProduct = useSelector(state=>state.products.userProdcuts.find(prod =>prod.id === prodId));
+  const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+  const [imageUrl, setImageUrl] = useState(
+    editedProduct ? editedProduct.imageUrl : ''
+  );
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState(
+    editedProduct ? editedProduct.description : ''
+  );
 
+  const submitHandler = useCallback(() => {
+    if (editedProduct) {
+      dispatch(
+        productsActions.updateProduct(prodId, title, description, imageUrl)
+      );
+    } else {
+      dispatch(
+        productsActions.createProduct(title, description, imageUrl, +price)
+      );
+    }
+    props.navigation.goBack();
+  }, [dispatch, prodId, title, description, imageUrl, price]);
 
-const [title,setTitle] =useState(editedProduct ? editedProduct.title:'');
-const [imageUrl,setimageUrl] =useState(editedProduct ? editedProduct.imageUrl:'');
-const [price,setprice] =useState('');
-const [description,setdescription] =useState(editedProduct ? editedProduct.description:'');
+  useEffect(() => {
+    props.navigation.setParams({ submit: submitHandler });
+  }, [submitHandler]);
 
-
-const submitHandler = useCallback(() =>{
-    
-},[]);
-
-useEffect(()=>{
-props.navigation.setParams({submit:submitHandler});
-},[submitHandler]);
-
-    return (
-        <ScrollView>
-            <View style={styles.form}>
-                <View style={styles.fromControl}>
-                    <Text style={styles.label}>Title</Text>
-                    <TextInput style={styles.input} value={title} onChange={text =>setTitle(text)} />
-                </View>
-
-                <View style={styles.fromControl}>
-                    <Text style={styles.label}>Image Url</Text>
-                    <TextInput style={styles.input} value={imageUrl} onChange={text =>setimageUrl(text)}/>
-                </View>
-
-                {editedProduct ? null : <View style={styles.fromControl}>
-                    <Text style={styles.label}>Price</Text>
-                    <TextInput style={styles.input} value={price} onChange={text =>setprice(text)}/>
-                </View>}
-
-                <View style={styles.fromControl}>
-                    <Text style={styles.label}>Description</Text>
-                    <TextInput style={styles.input} value={description} onChange={text =>setdescription(text)}/>
-                </View>
-            </View>
-        </ScrollView>
-
-    );
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            style={styles.input}
+            value={title}
+            onChangeText={text => setTitle(text)}
+          />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput
+            style={styles.input}
+            value={imageUrl}
+            onChangeText={text => setImageUrl(text)}
+          />
+        </View>
+        {editedProduct ? null : (
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput
+              style={styles.input}
+              value={price}
+              onChangeText={text => setPrice(text)}
+            />
+          </View>
+        )}
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={styles.input}
+            value={description}
+            onChangeText={text => setDescription(text)}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
-EditProductScreen.navigationOptions = navData =>{
-    const submitFn = navData.navigation.getParam('submit');
-    return{
-        headerTitle:navData.nagvigation.getParam('productId') ? 'Edit Product' : 'Add Product',
-        headerRight:(
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-              title="Save"
-              iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
-              onPress={(submitFn)}
-            />
-          </HeaderButtons>
-        )
-    };
+EditProductScreen.navigationOptions = navData => {
+  const submitFn = navData.navigation.getParam('submit');
+  return {
+    headerTitle: navData.navigation.getParam('productId')
+      ? 'Edit Product'
+      : 'Add Product',
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Save"
+          iconName={
+            Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+          }
+          onPress={submitFn}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
-    form:{margin:20},
-    fromControl:{
-        width:'100%'
-    },
-    label:{
-        fontFamily:'open-sans-bold',
-        marginVertical:8
-    },
-    input:{
-        paddingHorizontal:2,
-        paddingVertical:5,
-        borderBottomColor:'#ccc',
-        borderBottomWidth:1
-    }
+  form: {
+    margin: 20
+  },
+  formControl: {
+    width: '100%'
+  },
+  label: {
+    fontFamily: 'open-sans-bold',
+    marginVertical: 8
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  }
 });
-
-
-
 
 export default EditProductScreen;
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
+import { FlatList, Button, Platform, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -12,9 +12,22 @@ const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
   const dispatch = useDispatch();
 
-const editProductHandler = (id) =>{
-props.navigation.navigate('EditProduct',{ productId:id});
-};
+  const editProductHandler = id => {
+    props.navigation.navigate('EditProduct', { productId: id });
+  };
+
+  const deleteHandler = (id) => {
+    Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+      { text: 'No', style: 'default' },
+      {
+        text: 'Yes',
+        style: 'destructive',
+        onPress: () => {
+          dispatch(productsActions.deleteProduct(id));
+        }
+      }
+    ]);
+  };
 
   return (
     <FlatList
@@ -25,17 +38,21 @@ props.navigation.navigate('EditProduct',{ productId:id});
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onSelect={() => { editProductHandler(itemData.item.id);}}
+          onSelect={() => {
+            editProductHandler(itemData.item.id);
+          }}
         >
-          <Button color={Colors.primary} title="Edit" onPress={() => {
+          <Button
+            color={Colors.primary}
+            title="Edit"
+            onPress={() => {
               editProductHandler(itemData.item.id);
-          }} />
+            }}
+          />
           <Button
             color={Colors.primary}
             title="Delete"
-            onPress={() => {
-              dispatch(productsActions.deleteProduct(itemData.item.id));
-            }}
+            onPress={deleteHandler.bind(this, itemData.item.id)}
           />
         </ProductItem>
       )}
@@ -58,16 +75,16 @@ UserProductsScreen.navigationOptions = navData => {
       </HeaderButtons>
     ),
     headerRight: (
-        <HeaderButtons HeaderButtonComponent={HeaderButton}>
-          <Item
-            title="Add"
-            iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
-            onPress={() => {
-              navData.navigation.navigate('EditProduct');
-            }}
-          />
-        </HeaderButtons>
-      ),
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          onPress={() => {
+            navData.navigation.navigate('EditProduct');
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 
